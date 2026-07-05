aa7bb5a1d57844267eba84b2cdada99a7993434d
dami123-del <d.oluwadami@alustudent.com>
2026-07-05 11:48:42 -0700

    regular_expressios-8

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\from:(.+)\]\s\[to:(.+)\]\s\[flags:(.+)\]\s\[ms/).join(",")
+
+# 1. Capture the log line passed as the first argument
+log_line = ARGV[0]
+
+# 2. Extract the values using regex capture groups
+# .scan returns an array of matches; we take the first element [0]
+sender   = log_line.scan(/\[from:(.*?)\]/)[0][0]
+receiver = log_line.scan(/\[to:(.*?)\]/)[0][0]
+flags    = log_line.scan(/\[flags:(.*?)\]/)[0][0]
+
+# 3. Output them in the required format: [SENDER],[RECEIVER],[FLAGS]
+puts "#{sender},#{receiver},#{flags}"
